31c14354ba96d4d13211f8c4ab137f62f4ea9524
David Hinrichs <dvd.hnrchs@gmail.com>
2022-11-27 02:05:22 +0100

    initial commit

---
+++ utils.py
@@ -0,0 +1,35 @@
+from sqlalchemy import create_engine
+import random, string
+
+
+def connection_pool():
+    pass
+
+
+def solve(names_constraints):
+    givers = list(names_constraints.keys())
+    receivers = givers[1:] + [givers[0]]
+    givers_receivers = dict(zip(givers, receivers))
+    return givers_receivers
+
+
+def convert_text_to_names(txt):
+    names = txt.split(',')
+    names = [n.split()[0] for n in names if len(n)>0]
+    return names
+
+
+def get_gift_recipient(recipient_code):
+    return "Alice"
+
+
+def insert_solution_in_db(names_pseudonyms):
+    pass
+
+
+def get_n_random_identifiers(n):
+    letters = string.ascii_uppercase
+    n_identifiers = []
+    for _ in range(n):
+        n_identifiers.append(''.join(random.choice(letters) for i in range(5)))
+    return n_identifiers
